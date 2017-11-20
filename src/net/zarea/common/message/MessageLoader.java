9b9df13dd9ca929dd5939e7bab99a7eadcc206a4
zekuzeku <zeku.unknown@gmail.com>
2017-11-20 22:12:51 +0900

    メッセージローダークラスのコメントを追加

--- src/net/zarea/common/message/MessageLoader.java
+++ src/net/zarea/common/message/MessageLoader.java
@@ -3,23 +3,45 @@ package net.zarea.common.message;
 import java.io.FileInputStream;
 import java.util.Properties;
 
+/**
+ * メッセージローダークラス
+ * @author zeku
+ *
+ */
 public class MessageLoader {
 
-    private Properties msgPorp;
+    /** メッセージプロパティ */
+    private Properties msgPorp = null;
 
+    /**
+     * コンストラクタ
+     */
     public MessageLoader() {
 
+        // メッセージプロパティがnullの場合
         if (msgPorp == null) {
+
+            // メッセージプロパティを生成する。
             msgPorp = new Properties();
+
+            // プロパティファイルをStreamに読み込む。
             try (FileInputStream fis = new FileInputStream("D:/message.properties");) {
+
+                // メッセージプロパティにロードする。
                 msgPorp.load(fis);
+
+            // 予期せぬ例外が発生した場合
             } catch (Exception e) {
-                // TODO エラー処理
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * メッセージを取得する。
+     * @param messageId
+     * @return
+     */
     public String getMessage(String messageId) {
         return msgPorp.get(messageId).toString();
     }
